dcd4ca2d7f203e03d67eb7d8924d2b2c759fb0c3
Thomas <thomasjgouzecky@gmail.com>
2025-05-14 20:56:24 -0400

    Add an array to choose the foods from randomly

--- Lab 1/js/index.js
+++ Lab 1/js/index.js
@@ -3,3 +3,4 @@ let age;
 let isSuperhero;
 let specialPowers;
 let favoriteFood;
+const listOfFoods = ["Pizza", "Sushi", "Tacos", "Pasta", "Burgers", "Ice Cream", "Fried Chicken", "Ramen", "Steak", "Chocolate"];
