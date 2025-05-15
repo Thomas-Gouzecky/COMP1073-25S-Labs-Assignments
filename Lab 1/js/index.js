fbe77880d085ff91aa8938192b56665f555ac0ba
Thomas <thomasjgouzecky@gmail.com>
2025-05-14 21:23:24 -0400

    Added the variables for the buttons and set placeholders for the inputs

--- Lab 1/js/index.js
+++ Lab 1/js/index.js
@@ -2,10 +2,17 @@ let characterName = `The Incredible Burrito Boy`;
 let age = Math.ceil(Math.random()*100);
 let isSuperhero = true;
 let specialPowers = ["X-Ray Vision", "Mind Reading", "Teleportation", "Telekinesis", "Flight", "Invisibility", "Super Strength", "Immortality"];
-let favoriteFood = listOfFoods[9];
 const listOfFoods = ["Pizza", "Sushi", "Tacos", "Pasta", "Burgers", "Ice Cream", "Fried Chicken", "Ramen", "Steak", "Burritos"];
+let favoriteFood = listOfFoods[9];
 
 let nameField = document.querySelector('#name');
 let ageField = document.querySelector('#age');
 let favFoodField = document.querySelector('#favFood');
 let heroDescription = document.querySelector('#heroDescription');
+
+let randomButton = document.querySelector('button#randomButton');
+let createButton = document.querySelector('button');
+
+nameField.placeholder = characterName;
+ageField.placeholder = age;
+favFoodField.placeholder = favoriteFood;
