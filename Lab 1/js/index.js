16a8631033f01ec153724b1ea531f8c75ac01f16
Thomas <thomasjgouzecky@gmail.com>
2025-05-14 21:13:18 -0400

    Added variables that hold the inputs and paragraph

--- Lab 1/js/index.js
+++ Lab 1/js/index.js
@@ -4,3 +4,8 @@ let isSuperhero = true;
 let specialPowers = ["X-Ray Vision", "Mind Reading", "Teleportation", "Telekinesis", "Flight", "Invisibility", "Super Strength", "Immortality"];
 let favoriteFood = listOfFoods[9];
 const listOfFoods = ["Pizza", "Sushi", "Tacos", "Pasta", "Burgers", "Ice Cream", "Fried Chicken", "Ramen", "Steak", "Burritos"];
+
+let nameField = document.querySelector('#name');
+let ageField = document.querySelector('#age');
+let favFoodField = document.querySelector('#favFood');
+let heroDescription = document.querySelector('#heroDescription');
