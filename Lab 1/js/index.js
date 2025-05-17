70b384a285de300eb00439a08e789150cdff353f
Thomas <thomasjgouzecky@gmail.com>
2025-05-17 00:01:21 -0400

    formatted js using 'prettier' extension

--- Lab 1/js/index.js
+++ Lab 1/js/index.js
@@ -1,55 +1,73 @@
 let characterName = `The Incredible Burrito Boy`;
-let age = Math.ceil(Math.random()*100);
+let age = Math.ceil(Math.random() * 100);
 let isSuperhero = true;
-const specialPowers = ["X-Ray Vision", "Mind Reading", "Teleportation", "Telekinesis", "Flight", "Invisibility", "Super Strength", "Immortality"];
-const listOfFoods = ["Pizza", "Sushi", "Tacos", "Pasta", "Burgers", "Ice Cream", "Fried Chicken", "Ramen", "Steak", "Burritos"];
+const specialPowers = [
+  "X-Ray Vision",
+  "Mind Reading",
+  "Teleportation",
+  "Telekinesis",
+  "Flight",
+  "Invisibility",
+  "Super Strength",
+  "Immortality",
+];
+const listOfFoods = [
+  "Pizza",
+  "Sushi",
+  "Tacos",
+  "Pasta",
+  "Burgers",
+  "Ice Cream",
+  "Fried Chicken",
+  "Ramen",
+  "Steak",
+  "Burritos",
+];
 let favoriteFood = listOfFoods[9];
 
-let nameField = document.querySelector('#name');
-let ageField = document.querySelector('#age');
-let favFoodField = document.querySelector('#favFood');
-let heroDescription = document.querySelector('#heroDescription');
+let nameField = document.querySelector("#name");
+let ageField = document.querySelector("#age");
+let favFoodField = document.querySelector("#favFood");
+let heroDescription = document.querySelector("#heroDescription");
 
-let randomButton = document.querySelector('button#randomButton');
-let createButton = document.querySelector('button#create');
-let superpowersButton = document.querySelector('#superpowerToggle');
+let randomButton = document.querySelector("button#randomButton");
+let createButton = document.querySelector("button#create");
+let superpowersButton = document.querySelector("#superpowerToggle");
 
 nameField.value = characterName;
 ageField.value = age;
 favFoodField.value = favoriteFood;
 superpowersButton.checked = isSuperhero;
 
-function randomizeAttr(){
-    age = Math.ceil(Math.random()*100);
-    favoriteFood = listOfFoods[Math.floor(Math.random() * listOfFoods.length)];
-    ageField.value = age;
-    favFoodField.value = favoriteFood;
+function randomizeAttr() {
+  age = Math.ceil(Math.random() * 100);
+  favoriteFood = listOfFoods[Math.floor(Math.random() * listOfFoods.length)];
+  ageField.value = age;
+  favFoodField.value = favoriteFood;
 
-    if (Math.round(Math.random()) === 1){
-        isSuperhero = true;
-    }
-    else{
-        isSuperhero = false;
-    }
+  if (Math.round(Math.random()) === 1) {
+    isSuperhero = true;
+  } else {
+    isSuperhero = false;
+  }
 
-    superpowersButton.checked = isSuperhero;
+  superpowersButton.checked = isSuperhero;
 }
 
-function createCharacter(){
+function createCharacter() {
+  characterName = nameField.value;
+  age = ageField.value;
+  favoriteFood = favFoodField.value;
+  isSuperhero = superpowersButton.checked;
 
-    characterName = nameField.value;
-    age = ageField.value;
-    favoriteFood = favFoodField.value;
-    isSuperhero = superpowersButton.checked;
+  let description = `Meet the ${age} year old, ${favoriteFood} loving, ${characterName}!`;
 
-    let description = `Meet the ${age} year old, ${favoriteFood} loving, ${characterName}!`;
-
-    if(isSuperhero){
-        let power = specialPowers[Math.floor(Math.random() * specialPowers.length)];
-        description += ` With the power of ${power}, you don't want to mess with them!`;
-    }
-    heroDescription.textContent = description;
+  if (isSuperhero) {
+    let power = specialPowers[Math.floor(Math.random() * specialPowers.length)];
+    description += ` With the power of ${power}, you don't want to mess with them!`;
+  }
+  heroDescription.textContent = description;
 }
 
-createButton.addEventListener('click', createCharacter);
-randomButton.addEventListener('click', randomizeAttr);
+createButton.addEventListener("click", createCharacter);
+randomButton.addEventListener("click", randomizeAttr);
