e1a50c8eb81e917b2dd8a5c2f944ae06ad74d3a4
Thomas <thomasjgouzecky@gmail.com>
2025-05-29 21:35:36 -0400

    replaces 'Bob' with the users inputed name using regex

--- Lab 2/js/silly-story.js
+++ Lab 2/js/silly-story.js
@@ -54,6 +54,7 @@ function result() {
 
 	/* STEP 10: If the user has typed a name in the customName field, replace the name 'Bob' in the story with whatever they typed */
 	if (customName.value != "") {
+		newStory = newStory.replace(/Bob/g, customName.value);
 	}
 	/* STEP 11: If the metric radio button has been checked, we need to convert the temperature and mass numbers in the story */
 	if (document.getElementById("metric").checked) {
