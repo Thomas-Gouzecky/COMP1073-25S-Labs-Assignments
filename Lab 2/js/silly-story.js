8e106fbcd04b35d9f8edcf0bcedf99d2d9878336
Thomas <thomasjgouzecky@gmail.com>
2025-05-29 21:23:02 -0400

    fortmatted

--- Lab 2/js/silly-story.js
+++ Lab 2/js/silly-story.js
@@ -19,8 +19,8 @@ spontaneously combusted, rapidly sublimated, evaporated instantly
 // FUNCTIONS
 
 /* STEP 2: have a look at the following function - if you call this function and pass it an array, it will return one of the elements of that array randomly */
-function randomValueFromArray(array){
-	return array[Math.floor(Math.random()*array.length)];
+function randomValueFromArray(array) {
+	return array[Math.floor(Math.random() * array.length)];
 }
 
 /* STEP 6: Review the partially complete result() function below */
@@ -34,29 +34,22 @@ function result() {
 	/* STEP 9: Replace the three placeholders in the newStory string — :insertx:, :inserty:, and :insertz: — with the strings stored in xItem, yItem, and zItem. Each time, be sure to update the variable newStory (with +=). You might need to do one of the above replacements twice! */
 
 	/* STEP 10: If the user has typed a name in the customName field, replace the name 'Bob' in the story with whatever they typed */
-	if(customName.value != '') {
-
+	if (customName.value != "") {
 	}
 	/* STEP 11: If the metric radio button has been checked, we need to convert the temperature and mass numbers in the story */
-  if(document.getElementById("metric").checked) {
+	if (document.getElementById("metric").checked) {
 		// STEP 11a: Create a variable called weight and convert the 300lbs to kgs (1lb = 0.453592kg)
 		// STEP 11b: Replace the string 300 pounds with the updated weight in kg
-
 		// STEP 12a: Create a variable called temp and convert °F to °C ... the formula for conversion is °C = (°F - 32) x 5/9
-
 		// STEP 12b: Replace the string '94 fahrenheit' with the updated temperature in °C
-
-  }
+	}
 	/* STEP 13: Make the textContent property of the story variable (which references the paragraph) equal to newStory */
 
 	// The following line makes the paragraph visible
-	story.style.visibility = 'visible';
+	story.style.visibility = "visible";
 }
 
 // EVENT LISTENERS
 /* STEP 5: Add a click event listener to the randomize variable so that when the button it represents is clicked, the result() function is run. */
 
-
 // This lab based on the excellent assessment challenge at https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Silly_story_generator
-
-
