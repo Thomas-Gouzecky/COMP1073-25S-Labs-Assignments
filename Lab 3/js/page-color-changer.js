293cc534088af9af14fba7c7af71eebcbe2a5db2
Thomas <thomasjgouzecky@gmail.com>
2025-06-11 14:41:22 -0400

    created the function to change the background

--- Lab 3/js/page-color-changer.js
+++ Lab 3/js/page-color-changer.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
 	const slider = document.querySelectorAll("[type='range']");
 });
+
+function changeBackgroundColor() {
+	let red = document.getElementById("red").value;
+	let green = document.getElementById("green").value;
+	let blue = document.getElementById("blue").value;
+
+	document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+}
