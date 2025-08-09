1d03138cb7ed7a359379e9cf2246f95e43406ce3
Thomas <thomasjgouzecky@gmail.com>
2025-08-08 22:06:04 -0400

    dynamically assign the checked class to the buttons when they are selected

--- Assignment 2/js/main.js
+++ Assignment 2/js/main.js
@@ -2,6 +2,7 @@ import { Smoothie } from "./smoothie.js";
 
 const orderSmoothie = document.getElementById("orderBtn");
 
+// creates the smoothie object when the order smoothie button is clicked
 orderSmoothie.addEventListener("click", () => {
 	let customerName = document.getElementById("customerName").value;
 	let size = document.getElementById("size").value;
@@ -24,4 +25,33 @@ orderSmoothie.addEventListener("click", () => {
 	);
 
 	console.log(smoothieOrder);
+});
+
+// dynamically assign classes to labels to visually show what is clicked
+document.querySelectorAll("label").forEach((label) => {
+	let input = label.querySelector("input");
+	// not all labels have a child input
+	if (input != null) {
+		input.addEventListener("change", () => {
+			if (input.type === "radio") {
+				// remove all the radio buttons with the checked class
+				document
+					.querySelectorAll(`input[name="${input.name}"]`)
+					.forEach((radio) =>
+						radio.parentElement.classList.remove("checked")
+					);
+
+				// assign only the selected radio the class
+				if (input.checked) {
+					label.classList.add("checked");
+				}
+			}
+			// assign the label the checked class if its checked
+			if (input.checked) {
+				label.classList.add("checked");
+			} else {
+				label.classList.remove("checked");
+			}
+		});
+	}
 });
