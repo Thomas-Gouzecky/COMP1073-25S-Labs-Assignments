cc6199774feea9d935ddae9f7a087b1e1a993ec5
Thomas <thomasjgouzecky@gmail.com>
2025-08-11 19:41:14 -0400

    variables and event listener

---
+++ Assignment 3/js/main.js
@@ -0,0 +1,9 @@
+// get variables
+
+const infoBtn = document.getElementById("getInfo");
+const pokemonNameInput = document.getElementById("pokemonName");
+const pokedataDiv = document.getElementById("pokemonData");
+
+const baseURL = "https://pokeapi.co/api/v2/pokemon/";
+
+infoBtn.addEventListener("click", async () => {});
